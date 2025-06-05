1cdf2f6ea7798bec3bce8daa69d7a4dce774b2be
haexn <yoohe0321@gmail.com>
2025-06-05 15:46:14 +0900

    Create 00_image.py

---
+++ pages/00_image.py
@@ -0,0 +1,2 @@
+import streamlit as st
+st.image("https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExd3owMXB5NmEyamVjazNyMWduZ2tycDRoYnNyNjJsZDdrcWVkcTZmYyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/10UeedrT5MIfPG/giphy.gif", caption="happy", use colums
